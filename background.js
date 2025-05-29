97bd59a8fdebb0b6374cfe5cd16a5b374a7e1084
Immk-01 <mackmk@naver.com>
2025-05-29 23:05:39 +0900

    Delete background.js

--- background.js
+++
@@ -1,4 +0,0 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-const imgElement = document.getElementById("randomImage");
-imgElement.src = `img/${chosenImage}`;
